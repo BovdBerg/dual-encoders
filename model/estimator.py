e968c14a51bf54c79652ac0b0eb4481767efa8d9
Bo van den Berg <bvdb98@gmail.com>
2025-02-06 03:15:07 +0100

    Use self.n_docs instead of hardcoded 10

--- model/estimator.py
+++ model/estimator.py
@@ -168,7 +168,7 @@ class AvgEmbQueryEstimator(torch.nn.Module):
 
         # Initialize d_embs with zeros for invalid queries
         d_embs = self._get_top_docs_embs(valid_queries_df)
-        d_embs_full = torch.zeros((len(queries), 10, 768), device=self.device)
+        d_embs_full = torch.zeros((len(queries), self.n_docs, 768), device=self.device)
         d_embs_full[valid_queries_df.index] = d_embs
 
         # estimate query embedding as weighted average of q_emb and d_embs
