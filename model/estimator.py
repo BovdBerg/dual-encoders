0b6719fb27a08b5535680eeb914c9cd42adffc76
Bo van den Berg <bvdb98@gmail.com>
2025-02-06 16:21:02 +0100

    Try fix for outofbound error

--- model/estimator.py
+++ model/estimator.py
@@ -1,3 +1,4 @@
+import re
 from enum import Enum
 from typing import Dict, Optional, Sequence
 
@@ -8,7 +9,6 @@ import torch
 from fast_forward.index import Index
 from fast_forward.ranking import Ranking
 from transformers import AutoModel, AutoTokenizer
-import re
 
 EncodingModelBatch = Dict[str, torch.LongTensor]
 
@@ -116,23 +116,18 @@ class AvgEmbQueryEstimator(torch.nn.Module):
         ).to(self.device)
 
         # Encode top_docs_toks with doc_encoder and reshape
-        d_enc_embs = self.doc_encoder(d_toks)
+        d_enc_embs = self.doc_encoder(d_toks).last_hidden_state[:, 0, :]
         print(f"Shape of d_enc_embs: {d_enc_embs.shape}")
 
+        # Initialize d_embs with zeros
         d_embs = torch.zeros((len(queries), self.n_docs, 768), device=self.device)
-        q_groups = top_docs.groupby("query")
+        q_groups = top_docs.groupby("qid")
         q_nos = torch.tensor(q_groups.ngroup(), device=self.device)
-        ranks = torch.tensor(
-            q_groups["score"].rank(ascending=False, method="first").astype(int) - 1,
-            device=self.device,
-        )
-        d_embs[q_nos, ranks] = d_enc_embs
-        print(f"Shape of d_embs: {d_embs.shape}")
+        d_ranks = torch.tensor(q_groups.cumcount(), device=self.device)
+        d_embs[q_nos, d_ranks] = d_enc_embs
 
         # replace zeros in d_embs with emb at rank 0 (if n_top_docs was < self.n_docs for any queries)
-        d_embs[d_embs == 0] = (
-            d_embs[:, 0].unsqueeze(1).expand_as(d_embs)[d_embs == 0]
-        )
+        d_embs[d_embs == 0] = d_embs[:, 0].unsqueeze(1).expand_as(d_embs)[d_embs == 0]
         print(f"Shape of d_embs: {d_embs.shape}")
 
         return d_embs
