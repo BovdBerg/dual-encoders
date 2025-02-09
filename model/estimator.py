316d528dc39f1b4d4047e76b934181ae73de5f64
Bo van den Berg <bvdb98@gmail.com>
2025-02-09 21:39:00 +0100

    Renaming + documentation

--- model/estimator.py
+++ model/estimator.py
@@ -123,35 +123,34 @@ class AvgEmbQueryEstimator(torch.nn.Module):
         return top_docs_embs
 
     def forward(self, q_tokens: EncodingModelBatch) -> torch.Tensor:
-        input_ids = q_tokens["input_ids"]  # (batch_size, max_length)
-        batch_size, max_length = input_ids.size()
+        input_ids = q_tokens["input_ids"]  # (batch_size, max_len)
+        batch_size, max_len = input_ids.size()
 
         # Estimate lightweight query as (weighted) average of q_tok_embs, excluding padding
         q_tok_embs = self.tok_embs(input_ids)  # Get token embeddings
-        match self.tok_embs_w_method:  # q_tok_weights: (batch_size, max_length)
+        match self.tok_embs_w_method:  # q_tok_weights: (batch_size, max_len)
             case WEIGHT_METHOD.UNIFORM:
-                q_tok_weights = torch.ones_like(input_ids, dtype=torch.float) / max_length
+                q_tok_weights = torch.ones_like(input_ids, dtype=torch.float) / max_len
             case WEIGHT_METHOD.WEIGHTED:
                 q_tok_weights = self.tok_embs_weights[input_ids]
-        q_emb_1 = self.compute_weighted_average(q_tok_embs, q_tok_weights, q_tokens["attention_mask"])
+        q_tok_mask = q_tokens["attention_mask"]  # (batch_size, max_len)
+        q_emb_1 = self.compute_weighted_average(q_tok_embs, q_tok_weights, q_tok_mask)
         if self.q_only:
             return q_emb_1
 
         # Find top-ranked document embeddings
         queries = self.tokenizer.batch_decode(input_ids, skip_special_tokens=True)
-        d_embs = self._get_top_docs_embs(queries)
+        top_docs_embs = self._get_top_docs_embs(queries)
 
         # Estimate final query as (weighted) average of q_emb_1 ++ top_docs_embs
-        embs = torch.cat((q_emb_1.unsqueeze(1), d_embs), -2) # (batch_size, n_embs, emb_dim)
+        embs = torch.cat((q_emb_1.unsqueeze(1), top_docs_embs), -2) # (batch_size, n_embs, emb_dim)
         match self.embs_w_method:  # embs_weights: (batch_size, n_embs)
             case WEIGHT_METHOD.UNIFORM:
-                embs_weights = torch.ones_like((batch_size, self.n_embs), dtype=torch.float) / self.n_embs
+                embs_weights = torch.ones((batch_size, self.n_embs), device=self.device) / self.n_embs
             case WEIGHT_METHOD.WEIGHTED:
-                embs_weights = self.embs_weights # (n_embs)
-                embs_weights = embs_weights.unsqueeze(0).expand(batch_size, -1) # (batch_size, n_embs), repeated values
-        embs_mask = torch.zeros((batch_size, self.n_embs), device=self.device)  # (batch_size, n_embs), 1 for each non-zero emb
-        embs_mask[:, 0] = 1  # q_emb_1 is always non-zero
-        embs_mask[:, 1:] = torch.any(d_embs != 0, dim=-1)  # Check if any doc embs are non-zero
+                embs_weights = self.embs_weights.unsqueeze(0).expand(batch_size, -1) # (batch_size, n_embs), repeated values
+        embs_mask = torch.ones((batch_size, self.n_embs), device=self.device)  # (batch_size, n_embs), 1 for each non-zero emb
+        embs_mask[:, 1:] = torch.any(top_docs_embs != 0, dim=-1)  # Set empty doc embs to 0
         q_emb_2 = self.compute_weighted_average(embs, embs_weights, embs_mask)
         return q_emb_2
 
